9065662b59f59ac129b2fa730bff93eb418d8f43
omarjemli <omarjemli86@gmail.com>
2025-03-15 01:44:20 +0100

    updated news

--- lib/allNews.ts
+++ lib/allNews.ts
@@ -1,21 +1,21 @@
 export const allNews = [
   {
     id: 1,
-    title: "News 1",
-    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra scelerisque, duis tempus volutpat montes varius fusce urna nulla arcu hendrerit, cubilia massa metus tincidunt semper natoque erat sem. Conubia vitae in urna pulvinar tristique rutrum auctor quisque ad volutpat taciti curae molestie mauris, dignissim natoque purus aliquet sociis maecenas tempus proin dui metus rhoncus ultrices turpis.",
-    img: "/placeholder.jpg",
+    title: "Revolutionizing Gaming in Tunisia: The GNG x Orange Partnership",
+    desc:"Leveling Up Esports and Gaming in Tunisia: GNG x Orange Partnership We are thrilled to announce the official partnership between GnG esports and Orange Tunisie ! This collaboration represents a significant step forward for the esports and gaming community in Tunisia, paving the way for new opportunities, innovation, and growth. A Partnership to Empower Gamers This partnership is more than just a milestone; it is a commitment to supporting the vibrant esports and gaming ecosystem in Tunisia. By combining GNG s expertise in gaming with Orange s advanced technological capabilities, we aim to provide gamers with unmatched opportunities and experiences. Together, we are paving the way for a brighter future in gaming and esports, empowering players, teams, and fans alike.",
+    img: "/gng&orange.png",
   },
   {
     id: 2,
-    title: "News 2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra scelerisque, duis tempus volutpat montes varius fusce urna nulla arcu hendrerit, cubilia massa metus tincidunt semper natoque erat sem. Conubia vitae in urna pulvinar tristique rutrum auctor quisque ad volutpat taciti curae molestie mauris, dignissim natoque purus aliquet sociis maecenas tempus proin dui metus rhoncus ultrices turpis.",
-    img: "/placeholder.jpg",
+    title: "Introducing the Gaming Center on Max It,the ultimate shop for gamers",
+    desc:"The Orange Esports Arena platform is set to be a central hub for Tunisian gamers, offering: Weekly videos: Coaching content, tips, tricks, and insights to help gamers level up their skills in different disciplines. Monthly tournaments: Competitive events across various games, bringing together the best players in the region. Giveaways and challenges: Regular opportunities for gamers to win exciting prizes and showcase their abilities.",
+    img: "/catalogues.png",
   },
   {
     id: 3,
-    title: "News 3",
-    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit pharetra scelerisque, duis tempus volutpat montes varius fusce urna nulla arcu hendrerit, cubilia massa metus tincidunt semper natoque erat sem. Conubia vitae in urna pulvinar tristique rutrum auctor quisque ad volutpat taciti curae molestie mauris, dignissim natoque purus aliquet sociis maecenas tempus proin dui metus rhoncus ultrices turpis.",
-    img: "/placeholder.jpg",
+    title: "A Streaming Celebration with major influencers",
+    desc:"The Orange Gaming Hub event was not just an offline success; it was also streamed live on Twitch and YouTube, featuring the dynamic duo Dahma & Loujey, with top-notch casting by the talented Smooth. This added a whole new dimension to the event, allowing gamers from all over to join the celebration virtually and experience the excitement firsthand. Replay link: https://www.twitch.tv/videos/2312369957",
+    img: "/gamecenter.png",
   },
   {
     id: 4,
--- lib/news.ts
+++ lib/news.ts
@@ -1,27 +1,32 @@
 export const news = [
   {
     id: "1",
-    img: "/banner1.jpg",
-    alt: "banner1",
+    img: "/gng&orange.png",
+    alt: "gng&orange",
   },
   {
     id: "2",
-    img: "/banner2.webp",
-    alt: "banner2",
+    img: "/gamecenter.png",
+    alt: "gamecenter",
   },
   {
     id: "3",
-    img: "/banner3.webp",
-    alt: "banner3",
+    img: "/catalogues.png",
+    alt: "catalogues",
   },
   {
     id: "4",
-    img: "/banner1.jpg",
-    alt: "banner2",
+    img: "/streaming.png",
+    alt: "streaming",
   },
   {
     id: "5",
-    img: "/banner2.webp",
-    alt: "banner2",
+    img: "/gng&orange.png",
+    alt: "gng",
+  },
+  {
+    id: "56",
+    img: "/gamecenter.png",
+    alt: "gamecenter",
   },
 ];
